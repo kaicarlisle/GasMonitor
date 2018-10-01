ee5c080c17d6a5a5220e39b9cffe0050014c638d
CARLISLE Kai <kcarlisle@europe.corp.altran.com>
2018-10-01 12:41:36 +0100

    reads messages from sqs, parses into MessageResponse objects

--- src/main/java/gasmon/Main.java
+++ src/main/java/gasmon/Main.java
@@ -1,8 +1,41 @@
 package gasmon;
 
-public class Main {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
 
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+
+public class Main {
+	
 	public static void main(String[] args) {
-		new GetLocationsJSON();
+		ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider(".aws/credentials", "default");
+		File locationsJSON;
+		Sensor[] sensors = new Sensor[0];
+		LocationsParser locationsParser;
+		SNSTopicReceiver topicReceiver;
+		List<String> messages;
+		
+		//get locations.json from aws
+		GetLocationsJSONFromAWS getLocationsFromAWS = new GetLocationsJSONFromAWS("src/main/resources/", credentialsProvider);
+		locationsJSON = getLocationsFromAWS.getLocationsFile();
+		
+		//parse locations.json into Sensors[]
+		try {
+			locationsParser = new LocationsParser(locationsJSON);
+			sensors = locationsParser.parse();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		//receive messages from sqs server
+		topicReceiver = new SNSTopicReceiver(credentialsProvider);
+		messages = topicReceiver.getNextMessages(1);
+		for (String message : messages) {
+			//parse messages into MessageResponse objects
+			System.out.println(message);
+		}
+		topicReceiver.deleteQueue();
 	}
 }
